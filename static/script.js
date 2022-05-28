dfdd6f1092833ca4c453c4c6ecc385c12da3ef0c
Caio Giovani Malavazi Dariva <cgmdariva@outlook.com>
2022-05-28 01:14:50 -0300

    starting script.js
    script for change list to X, but is not working yet

---
+++ static/script.js
@@ -0,0 +1,11 @@
+/* Close lateral menu in mobile and Open in bigger screens */
+
+const menuMobile = document.querySelector('.menu-mobile');
+const body = document.querySelector('body');
+
+menuMobile.addEventListener('click', () => {
+    menuMobile.classList.contains("bi-list")    
+        ? menuMobile.classList.replace("bi-list", "bi-x")
+        : menuMobile.classList.replace("bi-x", "bi-list");
+    body.classList.toggle(menu-nav-active);
+});
