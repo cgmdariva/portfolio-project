ae2beca35ae8d347a1cb000200777fd608688099
Caio Giovani Malavazi Dariva <cgmdariva@outlook.com>
2022-05-30 22:31:11 -0300

    fix script call in index
    And fix menu-nav-active in script.js

--- static/script.js
+++ static/script.js
@@ -1,11 +1,12 @@
 /* Close lateral menu in mobile and Open in bigger screens */
 
-const menuMobile = document.querySelector('.menu-mobile');
+const menuMobile = document.querySelector(".menu-mobile");
 const body = document.querySelector('body');
 
-menuMobile.addEventListener('click', () => {
+menuMobile.addEventListener("click", () => {
     menuMobile.classList.contains("bi-list")    
         ? menuMobile.classList.replace("bi-list", "bi-x")
         : menuMobile.classList.replace("bi-x", "bi-list");
-    body.classList.toggle(menu-nav-active);
+    body.classList.toggle("menu-nav-active");
 });
+
