c1ecebb290a96982db1e09862528daf4db477fea
Caio Giovani Malavazi Dariva <cgmdariva@outlook.com>
2022-05-30 22:38:38 -0300

    starting section "sobre" and prettier
    prettier all code, and starting section "sobre" with html and bootsdtrap

--- static/script.js
+++ static/script.js
@@ -1,12 +1,11 @@
-/* Close lateral menu in mobile and Open in bigger screens */
+/* Close lateral menu in mobile, open in bigger screens and switch list to x */
 
 const menuMobile = document.querySelector(".menu-mobile");
-const body = document.querySelector('body');
+const body = document.querySelector("body");
 
 menuMobile.addEventListener("click", () => {
-    menuMobile.classList.contains("bi-list")    
-        ? menuMobile.classList.replace("bi-list", "bi-x")
-        : menuMobile.classList.replace("bi-x", "bi-list");
-    body.classList.toggle("menu-nav-active");
+  menuMobile.classList.contains("bi-list")
+    ? menuMobile.classList.replace("bi-list", "bi-x")
+    : menuMobile.classList.replace("bi-x", "bi-list");
+  body.classList.toggle("menu-nav-active");
 });
-
