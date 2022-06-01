b7f2b102a6c674ee3e6cd5a4daa688c8fbb44cca
Caio Giovani Malavazi Dariva <cgmdariva@outlook.com>
2022-05-31 22:27:04 -0300

    section "habilidades" and script
    script for close menu lateral and switch icon, x to list. Used bar progress from bootstrap.

--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
-/* Close lateral menu in mobile, open in bigger screens and switch list to x */
+/* Close lateral menu in mobile, open in bigger screens and switch icon list to x */
 
 const menuMobile = document.querySelector(".menu-mobile");
 const body = document.querySelector("body");
@@ -9,3 +9,16 @@ menuMobile.addEventListener("click", () => {
     : menuMobile.classList.replace("bi-x", "bi-list");
   body.classList.toggle("menu-nav-active");
 });
+
+/* Close lateral menu when one iten is clicked and change the icon to list again */
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item => {
+  item.addEventListener("click", () => {
+    if (body.classList.contains("menu-nav-active")) {
+      body.classList.remove("menu-nav-active")
+      menuMobile.classList.replace("bi-x", "bi-list")
+    }
+  })
+})
